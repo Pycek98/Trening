8075aa7c3373d6276f19a7d7894d04b1e41aec6e
Pycek98 <73182869+Pycek98@users.noreply.github.com>
2021-02-22 21:42:02 +0100

    Add files via upload

--- 8 - Tic Tac Toe/main.js
+++ 8 - Tic Tac Toe/main.js
@@ -1,44 +1,99 @@
+// Stałe, zmienne, elementy.
 const resetBtn = document.querySelector(".restart");
 const gameCells = document.querySelectorAll(".gridCell");
 const turnSign = document.querySelector("#turnSpan");
-turnSign.innerText = "X"
-
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
 let reloadFlag = false;
 let flagTurn = true;
+gameCellsValue = ["", "", "", "", "", "", "", "", ""];
+let ifGameEnd = false;
 
+// Funkcja określająca turę i zawartość komunikatu ew wygranej.
 
-gameCells.forEach(gameCell => {
-    gameCell.addEventListener("click", ()=>{
-        if(gameCell.innerText == ""){
-            if(flagTurn){
-                gameCell.innerText = "X";
+function whosTurn () {
+    if (flagTurn){
+        turnSign.innerText = "O" ;
+    }
+    else {
+        turnSign.innerText = "X" ;
+    }
+};
+
+// Funkcja zmieniająca rundę.
+
+function turn() {
+    flagTurn = !flagTurn;
+    whosTurn();
+};
+
+// Blokowanie komórek
+
+function block() {
+    gameCells.forEach((cell)=>{
+        cell.style.pointerEvents = "none";
+    })
+};
+
+// Sprawdzamy, czy gracz wygrał.
+
+function ifWin() {
+    if(gameCellsValue[0]==gameCellsValue[1]&&gameCellsValue[1]==gameCellsValue[2]&&gameCellsValue[2]!= ""){
+        ifGameEnd = true;
+    }
+    if(gameCellsValue[3]==gameCellsValue[4]&&gameCellsValue[4]==gameCellsValue[5]&&gameCellsValue[5]!= ""){
+        ifGameEnd = true;
+    }
+    if(gameCellsValue[6]==gameCellsValue[7]&&gameCellsValue[7]==gameCellsValue[8]&&gameCellsValue[8]!= ""){
+        ifGameEnd = true;
+    }
+    if(gameCellsValue[0]==gameCellsValue[3]&&gameCellsValue[3]==gameCellsValue[6]&&gameCellsValue[6]!= ""){
+        ifGameEnd = true;
+    }
+    if(gameCellsValue[1]==gameCellsValue[4]&&gameCellsValue[4]==gameCellsValue[7]&&gameCellsValue[7]!= ""){
+        ifGameEnd = true;
+    }
+    if(gameCellsValue[2]==gameCellsValue[5]&&gameCellsValue[5]==gameCellsValue[8]&&gameCellsValue[8]!= ""){
+        ifGameEnd = true;
+    }
+    if(gameCellsValue[0]==gameCellsValue[4]&&gameCellsValue[4]==gameCellsValue[8]&&gameCellsValue[8]!= ""){
+        ifGameEnd = true;
+    }
+    if(gameCellsValue[6]==gameCellsValue[4]&&gameCellsValue[4]==gameCellsValue[2]&&gameCellsValue[2]!= ""){
+        ifGameEnd = true;
+    }
+};
+
+// Na kliknięcie dodajemy O i X oraz używamy ifWin(); .
+gameCells.forEach((cell, i) => {
+    cell.addEventListener("click", ()=>{
+        if(cell.innerText==""){
+            if (flagTurn) {
                 turnSign.innerText = "O";
-                flagTurn = !flagTurn;
+                cell.innerText = turnSign.innerText;
+                gameCellsValue[i] = turnSign.innerText;
             }
             else {
-                gameCell.innerText = "O";
                 turnSign.innerText = "X";
-                flagTurn = !flagTurn;
+                cell.innerText = turnSign.innerText;
+                gameCellsValue[i] = turnSign.innerText;
             }
-        }
-        else {
-            alert("Wybierz wolne pole!")
+            ifWin();
+            if(ifGameEnd){
+                h1.innerText = `Player ${turnSign.innerText} wins!`;
+                h1.style.fontSize = "60px";
+                p.innerText = "Game Over!";
+                block();
+            }
+            else{
+            turn();
+            }
         }
     })
     
 });
 
-
-
-
-
-
-
-
-
-
-
-
+// Guzik Restart.
 
 resetBtn.addEventListener("click", () => {
     reloadFlag = true
